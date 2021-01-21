0cb89a25dfb6c43e355fece926be029a5ecfcbfc
MrYdag <ydag88@gmail.com>
2021-01-21 08:58:42 +0100

    version 2

--- main.cpp
+++ main.cpp
@@ -43,9 +43,52 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage &image, TGAColor color){
+
+    //On prend en compte le cas où nous avont un triangle plat
+    if (p1.y == p2.y && p1.y == p3.y) return;
+
+    //On trie les points de haut en bas
+    if(p1.y > p2.y) std::swap(p1,p2);
+    if(p1.y > p3.y) std::swap(p1,p3);
+    if(p2.y > p3.y) std::swap(p2,p3);
+
+    //Calcul la distance entre le point le plus haut et le point le plus bas
+    int total_height = p3.y-p1.y;
+
+    //Colorie la partie basse du triangle
+    for(int y = p1.y; y < p2.y; y++ ){
+        int segment_height = p2.y-p1.y+1;
+        float alpha = (float)(y-p1.y)/total_height;
+        float beta = (float)(y-p1.y)/segment_height;
+        Vec2i A = p1 + (p3-p1)*alpha;
+        Vec2i B = p1 + (p2-p1)*beta;
+        if(A.x>B.x) std::swap(A,B);
+        for(int j=A.x; j<=B.x;j++){
+            image.set(j,y,color);
+        }
+    }
+
+    //Colorie le reste du triangle
+    for(int y = p2.y; y < p3.y; y++ ){
+        int segment_height = p3.y-p2.y+1;
+        float alpha = (float)(y-p1.y)/total_height;
+        float beta = (float)(y-p2.y)/segment_height;
+        Vec2i A = p1 + (p3 - p1) * alpha;
+        Vec2i B = p2 + (p3-p2) * beta;
+        if(A.x>B.x) std::swap(A,B);
+        for(int j=A.x; j<=B.x; j++){
+            image.set(j,y,color);
+        }
+    }
+
+
+
+
+    /*
     line(p1.x,p1.y,p2.x,p2.y, image,color);
     line(p3.x,p3.y,p2.x,p2.y, image,color);
     line(p1.x,p1.y,p3.x,p3.y, image,color);
+     */
 }
 
 
@@ -65,7 +108,7 @@ int main(int argc, char** argv) {
         int y1 = (v2.y+1.)*height/2.;
         int x2 = (v3.x+1.)*width/2.;
         int y2 = (v3.y+1.)*height/2.;
-        triangle(Vec2i(x0, y0), Vec2i(x1, y1), Vec2i(x2, y2),image,white);
+        triangle(Vec2i(x0, y0), Vec2i(x1, y1), Vec2i(x2, y2),image,TGAColor(rand()%255, rand()%255, rand()%255, 255));
     }
     image.write_tga_file("output.tga");
 
