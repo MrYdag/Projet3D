3bd0b6c632bed026e5247f4320e2a66cbf5803cf
MrYdag <ydag88@gmail.com>
2021-02-11 09:39:22 +0100

    version 6

--- main.cpp
+++ main.cpp
@@ -13,8 +13,50 @@ const int height = 800;
 const int depth = 255;
 float *zbuffer = new float [width*height];
 
+Vec3f light_dir(0,0,-1);
+Vec3f camera(0,0,3);
+
 //test
 TGAImage texture;
+
+///////////////////////
+
+Vec3i m2v(Matrix m) {
+    float rapport = m[3][0];
+    return Vec3i(m[0][0]/rapport, m[1][0]/rapport, m[2][0]/rapport);
+}
+/**
+ * Ceci est extremement moche, mais vu qu'on ne peux pas cast un Vec3f en Vec3i ...
+ * @param m
+ * @param a
+ * @return
+ */
+Vec3f m2v(Matrix m, std::string a) {
+    float rapport = m[3][0];
+    return Vec3f(m[0][0]/rapport, m[1][0]/rapport, m[2][0]/rapport);
+}
+
+Matrix v2m(Vec3f v) {
+    Matrix m(4, 1);
+    m[0][0] = v.x;
+    m[1][0] = v.y;
+    m[2][0] = v.z;
+    m[3][0] = 1.f;
+    return m;
+}
+
+Matrix viewport(int x, int y, int w, int h) {
+    Matrix m = Matrix::identity(4);
+    m[0][3] = x+w/2.f;
+    m[1][3] = y+h/2.f;
+    m[2][3] = depth/2.f;
+
+    m[0][0] = w/2.f;
+    m[1][1] = h/2.f;
+    m[2][2] = depth/2.f;
+    return m;
+}
+/////////////////////////
 
 
 
@@ -51,7 +93,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 
-Vec3f barycentric(Vec3f p1, Vec3f p2, Vec3f p3, Vec3f P) {
+Vec3f barycentric(Vec3i p1, Vec3i p2, Vec3i p3, Vec3f P) {
     Vec3f u = cross(Vec3f(p3.x-p1.x, p2.x-p1.x, p1.x-P.x), Vec3f(p3.y-p1.y, p2.y-p1.y,p1.y-P.y));
     if (std::abs(u[2])<1) return Vec3f(-1,1,1);
     return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
@@ -60,18 +102,14 @@ Vec3f barycentric(Vec3f p1, Vec3f p2, Vec3f p3, Vec3f P) {
 //Triangle en coupant le triangle en deux et en coloriant les deux parties
 /*
 void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage &image, TGAColor color){
-
     //On prend en compte le cas où nous avont un triangle plat
     if (p1.y == p2.y && p1.y == p3.y) return;
-
     //On trie les points de haut en bas
     if(p1.y > p2.y) std::swap(p1,p2);
     if(p1.y > p3.y) std::swap(p1,p3);
     if(p2.y > p3.y) std::swap(p2,p3);
-
     //Calcul la distance entre le point le plus haut et le point le plus bas
     int total_height = p3.y-p1.y;
-
     //Colorie la partie basse du triangle
     for(int y = p1.y; y < p2.y; y++ ){
         int segment_height = p2.y-p1.y+1;
@@ -84,7 +122,6 @@ void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage &image, TGAColor color){
             image.set(j,y,color);
         }
     }
-
     //Colorie le reste du triangle
     for(int y = p2.y; y < p3.y; y++ ){
         int segment_height = p3.y-p2.y+1;
@@ -97,7 +134,6 @@ void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage &image, TGAColor color){
             image.set(j,y,color);
         }
     }
-
 }
 */
 
@@ -110,17 +146,17 @@ void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage &image, TGAColor color){
  * @param image
  * @param color couleur du triangle
  */
-void triangle(Vec3f p1,Vec3f p2,Vec3f p3,float *zbuffer, Vec3f ptsTexture[3],TGAImage &image, float intensity) {
+void triangle(Vec3i p1,Vec3i p2,Vec3i p3,float *zbuffer, Vec3f ptsTexture[3],TGAImage &image, float intensity) {
 
-    Vec3f pts[3] = {p1,p2,p3};
+    Vec3i pts[3] = {p1,p2,p3};
 
     Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
     Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
     Vec2f clamp(image.get_width()-1, image.get_height()-1);
     for (int i=0; i<3; i++) {
         for (int j=0; j<2; j++) {
-            bboxmin[j] = std::max(0.f,        std::min(bboxmin[j], pts[i][j]));
-            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+            bboxmin[j] = std::max(0.f,        std::min(bboxmin[j], float (pts[i][j])));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], float (pts[i][j])));
         }
     }
     Vec3f P;
@@ -138,6 +174,7 @@ void triangle(Vec3f p1,Vec3f p2,Vec3f p3,float *zbuffer, Vec3f ptsTexture[3],TGA
                 double v = bc_screen[0]*ptsTexture[0].y + bc_screen[1]*ptsTexture[1].y + bc_screen[2]*ptsTexture[2].y;
 
                 TGAColor color = texture.get(u*texture.get_width(),v*texture.get_height());
+                //TGAColor color = TGAColor(255,255,255);
                 image.set(P.x, P.y, color*intensity);
             }
         }
@@ -147,6 +184,10 @@ void triangle(Vec3f p1,Vec3f p2,Vec3f p3,float *zbuffer, Vec3f ptsTexture[3],TGA
 
 int main(int argc, char** argv) {
     TGAImage image(width, height, TGAImage::RGB);
+
+    Matrix Projection = Matrix::identity(4);
+    Matrix ViewPort   = viewport(width/8, height/8, width*3/4, height*3/4);
+    Projection[3][2] = -1.f/camera.z;
 
     texture.read_tga_file("obj/head.tga");
     texture.flip_vertically();
@@ -160,29 +201,29 @@ int main(int argc, char** argv) {
         std::vector<int> list = objet->getFace(i);
         std::vector<int> listTexture = objet->getFaceTexture(i);
 
-        Vec3f v1 = objet->getVertice(list[0]);
-        Vec3f v2 = objet->getVertice(list[1]);
-        Vec3f v3 = objet->getVertice(list[2]);
+        Vec3f v[3];
+
         Vec3f ptsTexture[3];
-        ptsTexture[0] = objet->getTexture(listTexture[0]);
-        ptsTexture[1] = objet->getTexture(listTexture[1]);
-        ptsTexture[2] = objet->getTexture(listTexture[2]);
-        int x0 = (v1.x+1.)*width/2.;
-        int y0 = (v1.y+1.)*height/2.;
-        int z0 = (v1.z+1)*depth/2;
-        int x1 = (v2.x+1.)*width/2.;
-        int y1 = (v2.y+1.)*height/2.;
-        int z1 = (v2.z+1)*depth/2;
-        int x2 = (v3.x+1.)*width/2.;
-        int y2 = (v3.y+1.)*height/2.;
-        int z2 = (v3.z+1)*depth/2;
+
+        Vec3i screen_coords[3];
+        Vec3f screen_coords_f[3];
+        for (int i = 0; i<3;i++){
+            v[i] = objet->getVertice(list[i]);
+
+            screen_coords[i] = m2v(ViewPort*Projection*v2m(v[i]));
+            screen_coords_f[i] = m2v(ViewPort*Projection*v2m(v[i]),"");
+
+            ptsTexture[i] = objet->getTexture(listTexture[i]);
+
+        }
 
         //Calcul de la lumiere
-        Vec3f n = cross ((v3-v1), (v2-v1));
+        Vec3f n = cross ((screen_coords_f[2]-screen_coords_f[0]), (screen_coords_f[1]-screen_coords_f[0]));
         n.normalize();
-        float intensity = n*light_dir;
+        float intensity = n *light_dir;
+
         if(intensity > 0){
-            triangle(Vec3f(x0, y0,z0), Vec3f(x1, y1,z1), Vec3f(x2, y2,z2),zbuffer, ptsTexture,image,intensity);
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2],zbuffer, ptsTexture,image,intensity);
         }
     }
     image.write_tga_file("output.tga");
